4ab852ada669591f7d9ead8641b453210d15e07c
Aziret Arlen <azerarlen312@gmail.com>
2021-08-21 18:22:14 +0600

    Модель объявлений

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.core.validators import MinLengthValidator
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils import timezone
 
@@ -11,96 +12,46 @@ STATUS_CHOICES = [
 ]
 
 
-class Article(models.Model):
+class Advert(models.Model):
     title = models.CharField(max_length=200, null=False, blank=False, verbose_name='Заголовок',
                              validators=[MinLengthValidator(10)])
-    text = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Текст')
+    photo_img = models.ImageField(null=True, blank=True, upload_to='advert_pics', verbose_name='Картинка')
+    text = models.TextField(max_length=3000, null=True, blank=True, verbose_name='Текст')
+    price = models.DecimalField(null=True, blank=True, verbose_name='Цена', max_digits=7, decimal_places=2, validators=[MinValueValidator(0)])
     author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
-                               related_name='articles', verbose_name='Автор')
-    status = models.CharField(max_length=15, choices=STATUS_CHOICES, default='new', verbose_name='Модерация')
-    tags = models.ManyToManyField('webapp.Tag', related_name='articles', blank=True, verbose_name='Теги')
+                               related_name='adverts', verbose_name='Автор')
+    category = models.ManyToManyField('webapp.Category', related_name='adverts', blank=True, verbose_name='Категории')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
     publish_at = models.DateTimeField(verbose_name="Время публикации", blank=True, default=timezone.now)
-    like_count = models.IntegerField(verbose_name="Счётчик лайков", default=0)
+    status = models.CharField(null=True, blank=True, max_length=15, choices=STATUS_CHOICES, default='new',
+                              verbose_name='Статус')
+    is_delete = models.BooleanField(default=False)
 
     def save(self, **kwargs):
         if not self.publish_at:
             if not self.pk:
                 self.publish_at = timezone.now()
             else:
-                self.publish_at = Article.objects.get(pk=self.pk).publish_at
+                self.publish_at = Advert.objects.get(pk=self.pk).publish_at
         super().save(**kwargs)
-
-    def liked_by(self, user):
-        likes = self.likes.filter(user=user)
-        return likes.count() > 0
 
     def __str__(self):
         return "{}. {}".format(self.pk, self.title)
 
     class Meta:
-        verbose_name = 'Статья'
-        verbose_name_plural = 'Статьи'
+        permissions = [('moderator', 'Модератор')]
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявлении'
 
 
-class Comment(models.Model):
-    article = models.ForeignKey('webapp.Article', related_name='comments',
-                                on_delete=models.CASCADE, verbose_name='Статья')
-    text = models.TextField(max_length=400, verbose_name='Комментарий')
-    author = models.ForeignKey(get_user_model(), on_delete=models.SET_DEFAULT, default=1,
-                               related_name='comments', verbose_name='Автор')
-    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
-    updated_at = models.DateTimeField(auto_now=True, verbose_name='Время изменения')
-    like_count = models.IntegerField(verbose_name="Счётчик лайков", default=0)
-
-    def liked_by(self, user):
-        likes = self.likes.filter(user=user)
-        return likes.count() > 0
-
-    def __str__(self):
-        return self.text[:20]
-
-    class Meta:
-        verbose_name = 'Комментарий'
-        verbose_name_plural = 'Комментарии'
-
-
-class Tag(models.Model):
-    name = models.CharField(max_length=31, verbose_name='Тег')
+class Category(models.Model):
+    name = models.CharField(max_length=31, verbose_name='Категории')
     created_at = models.DateTimeField(auto_now_add=True, verbose_name='Время создания')
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = 'Тег'
-        verbose_name_plural = 'Теги'
-
-
-class ArticleLike(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, 
-                             related_name='article_likes', verbose_name='Пользователь')
-    article = models.ForeignKey('webapp.Article', on_delete=models.CASCADE,
-                                related_name='likes', verbose_name='Статья')
-
-    def __str__(self):
-        return f'{self.user.username} - {self.article.title}'
-
-    class Meta:
-        verbose_name = 'Лайк'
-        verbose_name_plural = 'Лайки статей'
-
-
-class CommentLike(models.Model):
-    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE,
-                             related_name='comment_likes', verbose_name='Пользователь')
-    comment = models.ForeignKey('webapp.Comment', on_delete=models.CASCADE, verbose_name='Комментарий',
-                                related_name='likes')
-
-    def __str__(self):
-        return f'{self.user.username} - {self.comment.text}'
-
-    class Meta:
-        verbose_name = "Лайк"
-        verbose_name_plural = 'Лайки комментариев'
+        verbose_name = 'Категории'
+        verbose_name_plural = 'Категории'
